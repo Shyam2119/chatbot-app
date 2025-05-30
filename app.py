8b8b23b60fc0340c126bf5801bf017214c95c917
Shyam2119 <shyampattipu@gmail.com>
2025-05-30 20:44:31 +0530

    Initial commit: Flask chatbot with intents

--- app.py
+++ app.py
@@ -243,4 +243,4 @@ def get_quick_replies(intent):
     return quick_replies_map.get(intent, [])
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
